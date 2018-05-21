4de90f729344f486ec4875e404e1acac4dff5b0f
vaibhav <waxvapour@gmail.com>
2018-05-21 19:35:08 +0530

    day1

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+    res.render('index')
+})
+app.post('/', function (req, res) {
+  console.log(req.body.city);
+res.render('index');
+});
+app.listen(3000, function () {
+  console.log('Listening on port 3000!')
+})
